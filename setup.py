9163617985de15ef84dd45359630fd6b2cea2cc9
Codestian <mh.silvine@gmail.com>
2020-07-31 20:01:50 +0800

    fixed error exception

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ltapysg",
-    version="0.1.3",
+    version="0.1.5",
     author="Muhd Hakim",
     author_email="codestia@gmail.com",
     description="Asynchronous python library to access LTA's Datamall, primarily designed for Home Asssitant.",
